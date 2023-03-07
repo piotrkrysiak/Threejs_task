9c1d1b8a7aa5fb476718912bc5e81ecfb63bf749
Piotr Krysiak <plotrekhqpl@gmail.com>
2023-03-07 16:55:09 +0100

    Glb object placed & light set up

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const render = new THREE.WebGLRenderer();
 
@@ -21,8 +22,30 @@ camera.position.z = 10;
 const orbitControls = new OrbitControls(camera, render.domElement);
 orbitControls.update();
 
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
+const loader = new GLTFLoader();
+loader.load(
+  "assets/Object.glb",
+  (glb) => {
+    console.log(glb);
+    scene.add(glb.scene);
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  (error) => {
+    console.log("An error happened", error);
+  }
+);
+
+const colorLight = new THREE.Color("#F2F2F2");
+
+const light = new THREE.DirectionalLight(colorLight, 3);
+light.position.set(2, 2, 5);
+
+const pointLight = new THREE.PointLight(colorLight, 2);
+pointLight.position.set(-4, 2, 5);
+
+scene.add(light, pointLight);
 
 const animate = () => {
   requestAnimationFrame(animate);
