4241c5b25aebbaa9fc70994fc34eac0a86f88bcd
Piotr Krysiak <plotrekhqpl@gmail.com>
2023-03-07 16:44:42 +0100

    Three.js basic config added

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-console.log("three", THREE);
+const render = new THREE.WebGLRenderer();
+
+render.setSize(window.innerWidth, window.innerHeight);
+
+const app = document.getElementById("app");
+app && app.appendChild(render.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 10;
+
+const orbitControls = new OrbitControls(camera, render.domElement);
+orbitControls.update();
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  render.render(scene, camera);
+};
+
+render.setAnimationLoop(animate);
